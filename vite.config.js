05d861597b40994590eeb017e37085b5882796e2
YagraProk <zssl@mail.ru>
2023-06-26 20:23:03 +0700

    base cnf

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from "url";
 
 export default defineConfig({
   plugins: [vue()],
+  bade: "/neo-brutalism",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
